2eafc7a071390d94deddd4f7bd4dea228c1bfeed
vit9696 <vit9696@users.noreply.github.com>
2020-07-17 09:51:09 +0300

    Revert 3

--- Drivers/SataControllerDxe/SataController.c
+++ Drivers/SataControllerDxe/SataController.c
@@ -1127,6 +1127,9 @@ IdeInitCalculateMode (
   } else {
     (*SupportedModes)->UdmaMode.Valid = FALSE;
   }
+
+  (*SupportedModes)->MultiWordDmaMode.Valid = FALSE;
+
   DEBUG ((DEBUG_INFO, "IdeInitCalculateMode: UdmaMode = %x\n", (*SupportedModes)->UdmaMode.Mode));
 
   //
